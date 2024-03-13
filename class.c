a7b9062982fb5277e560087b239446768cab8623
kar1f <145224790+kar1f@users.noreply.github.com>
2024-03-13 15:52:22 +0530

    Create class.c

---
+++ class.c
@@ -0,0 +1,18 @@
+//Author:MD KARIF HASAN
+
+#include <iostream>
+using namespace std;
+class Ma202{
+public:
+
+    void messege()
+    {
+        cout <<"\n Classes are from 12th March"<<endl;
+
+    }
+};
+int main ()
+{
+    Ma202 m1;
+   m1.messege();
+}
